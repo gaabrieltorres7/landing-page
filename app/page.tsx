e364534bdf668198364ea2541314292767577047
Gabriel Torres <gaabrieltt7@gmail.com>
2023-06-30 22:15:45 -0300

    features

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import HeroSection from '@/components/HeroSection'
+import Features from '@/components/Features'
 
 export default function Home() {
   return (
     <>
     <HeroSection />
+    <Features />
     </>
   )
 }
